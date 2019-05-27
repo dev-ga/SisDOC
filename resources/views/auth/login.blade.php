6822f1f77a8606b4a8a20edd2f3db064429ec589
root <root@PR400525D9.intra.bcv>
2019-05-27 17:35:00 -0400

    Modificaciones varias

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,38 +9,9 @@
 <link href="{{ asset('fonts/fontawesome/css/fontawesome.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('fonts/fontawesome/css/brands.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('fonts/fontawesome/css/solid.css') }}" rel="stylesheet" type="text/css">
-    
 </head>
-<body>
-<div class="container h-100">
-    <div class="row align-items-center h-100">
-        <div class="col-md-4 mx-auto">
-            <div class="card">
-                <img class="card-img-top img-fluid login-icon" src="http://chittagongit.com/download/226043" alt="Card image cap">
-                <div class="card-body ">
-                    <h4 class="card-title text-center">Login</h4>
-                    <form class="" method="POST" action="{{ route('authvalidate.login') }}">
-                        @csrf {{-- Para mostrar los errores si enviamos en formulario vasio --}}
-                        <div class="form-group">
-                            <input type="text" name="email" id="email" value="{{ old('email') }}" class="form-control form-control-login " placeholder="E-mail">
-                            {!! $errors->first ('email', '<span class="badge badge-danger">:message</span>') !!}
-                        </div>
-                        <div class="form-group">
-                            <input type="password" name="password" id="password" class="form-control form-control-login" placeholder="Password">
-                            {!! $errors->first ('password', '<span class="badge badge-danger">:message</span>' ) !!}
-                        </div>
-                        <!-- Sign in button -->
-                    <button class="btn btn-info btn-block my-2" type="submit">Entrar</button>
-
-                    <!-- Register -->
-                    <p class="texto">No estas Resgistrado?<a href="{{ route('auth.registrousuarios') }}">  Registro</a></p>
-                    </form>
-                  </div>
-            </div>
-        </div>
-    </div>
-</div>
-    
+<body class="viewport">
+	
 <script src="{{ asset('js/jquery-3.4.1.js') }}"></script>
 <script src="{{ asset('js/jquery.slim.js') }}"></script>
 <script src="{{ asset('js/bootstrap.js') }}"></script>
--- resources/views/auth/registrousuarios.blade.php
+++ resources/views/auth/registrousuarios.blade.php
@@ -11,75 +11,75 @@
 <link href="{{ asset('fonts/fontawesome/css/solid.css') }}" rel="stylesheet" type="text/css">
     
 </head>
-<body>
-<div class="container-fluid h-100">
-    <div class="row h-100 ">
-        <div class="col-md-5 left d-flex align-self-center h-100">
-            <div class="card fondo-card-izquierdo">
-                <div class="card-body">
-                    <h1 class="card-title text-center card-texto-izquierda ">S!SDOC</h1>
-                    <h6 class="card-subtitle mb-2 text-muted text-center">Sistema de Documentos Interactivos</h6>
-                    <hr>
-                    <p class="card-text card-texto-izquierda ">
-                     Debe llenar en su totalidad el formulario de resgistro para poder interactuar de forma segura con la aplicación
-                        Si tiene alguna duda por favor contacte al administrador del Sistema o comuniquese con la Oficina de Tecnología.
-                    </p>
-                </div>
-            </div>  
-        </div>
-
-        <div class="col-md-7">
-            <div class="container-fluid h-100">
-            <div class="row h-100   right d-flex align-self-center">
-            
-        <div class="col-md-10 ">
-            <h3 class="card-title text-center">Formulario de Registro</h3>
-            
-            <form method="POST" action="{{ route('authvalidate.registro') }}"> 
-                
-                @csrf{{-- errores del formularios --}}
-
-                <div class="form-group "> 
-                    <label for="Nombre">Nombre Completo</label>
-                    <input type="text" name="nombre" class="form-control bordes" id="formGroupExampleInput" autofocus>
-                </div> 
-                <div class="form-group"> 
-                    <label for="apellido"> Primer Apellido</label>
-                    <input type="text" name="apellido" class="form-control bordes" id="formGroupExampleInput2">
-                </div>
-                <div class="form-group"> 
-                    <label for="cedula">Cedula de Identidad</label>
-                    <input type="text" name="cedula" class="form-control bordes" id="formGroupExampleInput" >
-                </div> 
-                <div class="form-group"> 
-                    <label for="organizacion_id">Organizacion</label>
-                    {{-- <input name="organizacion_id" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
-                </div> --}}
-                    <select name="organizacion_id" class="form-control estilosinputregistro mb-2 bordes">
-                        <option value="">-- Perteneces a...? --</option>
-                            @foreach($organizacion as $organizacion)
-                                <option value="{{ $organizacion->id }}">{{ $organizacion->descripcion_sigesp }}</option>
-                            @endforeach
-                    </select>
-                <div class="form-group"> 
-                    <label for="email">E-mail</label>
-                    <input type="email" name="email" class="form-control bordes" id="formGroupExampleInput" >
-                </div> 
-                <div class="form-group"> 
-                    <label for="password">Password</label>
-                    <input type="password" name="password" class="form-control bordes" id="formGroupExampleInput2" >
-                </div>
-                <!-- Sign in button -->
-                <button class=" estilosbutton btn btn-info btn-block my-5" type="submit">Registrar</button>
-            </form>
-            
-            </div>
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
+<body class="viewport">
     
+    <div class="container">
+        <div class="row">
+    <div class="col-md-4 py-5 bg-primary text-white text-center ">
+                <div class=" ">
+                    <div class="card-body">
+                        <img src="http://www.ansonika.com/mavia/img/registration_bg.svg" style="width:30%">
+                        <h2 class="py-3">Registration</h2>
+                        <p>Tation argumentum et usu, dicit viderer evertitur te has. Eu dictas concludaturque usu, facete detracto patrioque an per, lucilius pertinacia eu vel.
+
+</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8 py-5 border">
+                <h4 class="pb-4">Please fill with your details</h4>
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                          <input id="Full Name" name="Full Name" placeholder="Full Name" class="form-control" type="text">
+                        </div>
+                        <div class="form-group col-md-6">
+                          <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                        </div>
+                      </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <input id="Mobile No." name="Mobile No." placeholder="Mobile No." class="form-control" required="required" type="text">
+                        </div>
+                        <div class="form-group col-md-6">
+                                  
+                                  <select id="inputState" class="form-control">
+                                    <option selected>Choose ...</option>
+                                    <option> New Buyer</option>
+                                    <option> Auction</option>
+                                    <option> Complaint</option>
+                                    <option> Feedback</option>
+                                  </select>
+                        </div>
+                        <div class="form-group col-md-12">
+                                  <textarea id="comment" name="comment" cols="40" rows="5" class="form-control"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group">
+                            <div class="form-group">
+                                <div class="form-check">
+                                  <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+                                  <label class="form-check-label" for="invalidCheck2">
+                                    <small>By clicking Submit, you agree to our Terms & Conditions, Visitor Agreement and Privacy Policy.</small>
+                                  </label>
+                                </div>
+                              </div>
+                    
+                          </div>
+                    </div>
+                    
+                    <div class="form-row">
+                        <button type="button" class="btn btn-danger">Submit</button>
+                    </div>
+                </form>
+            </div>
+  </div>
+    </div>
+
+
+    
+   
 <script src="{{ asset('js/jquery-3.4.1.js') }}"></script>
 <script src="{{ asset('js/jquery.slim.js') }}"></script>
 <script src="{{ asset('js/bootstrap.js') }}"></script>
